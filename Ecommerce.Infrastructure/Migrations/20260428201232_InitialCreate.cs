a03501c643c181c07f1ff2891cacc8ca4bed16ce
Elisa Rodríguez <elisaholguin21@gmail.com>
2026-04-28 15:30:24 -0500

    Arquitectura limpia: eliminados binarios y agregadas migraciones de BD

---
+++ Ecommerce.Infrastructure/Migrations/20260428201232_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "pr_producto",
+                columns: table => new
+                {
+                    id_producto = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombre = table.Column<string>(type: "text", nullable: false),
+                    precio = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pr_producto", x => x.id_producto);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pr_producto");
+        }
+    }
+}
