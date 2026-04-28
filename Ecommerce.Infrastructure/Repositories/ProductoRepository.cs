bf20af030fce37af0efadea5c3bd664f2421124f
Elisa Rodríguez <elisaholguin21@gmail.com>
2026-04-28 15:59:52 -0500

    Carpeta repositorio creada para el proyecto

---
+++ Ecommerce.Infrastructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Repositories
+{
+    public class ProductoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> GuardarProducto(Producto producto)
+        {
+            _context.pr_producto.Add(producto);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
